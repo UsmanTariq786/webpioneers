ff8d22712961a46649615f6e6ced418001bc8490
muhammad-noman732 <iasknoman156@gmail.com>
2025-07-16 09:01:31 -0700

    Finished homepage partner websitedesign , wesitedevelopment brandIdentity processstepsection

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import BrandIdentity from "@/components/brandIdentity/BrandIdentity";
+import Button from "@/components/button/Button";
+import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
+import PartnersSection from "@/components/partnersSection/PartnersSection";
+import ProcessStepsSection from "@/components/processStepsSection/ProcessStepsSection";
+import Logo from "@/components/ui/Logo";
+import Reuse from "@/components/ui/Reuse";
+import WebsiteDesignFeature from "@/components/websiteDesignFeature/WebsiteDesignFeature";
+import WebsiteDevelopment from "@/components/websiteDevelopment/WebsiteDevelopment";
+import Image from "next/image";
+import Link from "next/link";
 
-const HomePage = () => {
+export default function Home() {
+
   return (
-    <div>
-      Home Page
+    <div className="min-h-screen bg-black">
+      <Header />
+      <Hero />
+      <PartnersSection />
+      <div className="flex justify-center items-center">
+      <video width="1000px" height="200px" controls 
+          className=" border rounded-2xl" >
+        <source src="video.mp4" type="video/mp4" />
+         Your browser does not support the video tag.
+      </video>
+      </div>
+      <ProcessStepsSection/>
+      <BrandIdentity/>
+      <WebsiteDesignFeature/>
+      <WebsiteDevelopment/>
+      
     </div>
-  )
+  );
 }
-
-export default HomePage
