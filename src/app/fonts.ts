211de4c21994483a2b19d11eaa184a24959d86b4
usmantariq <usman@alfain.tech>
2025-07-27 21:46:54 +0500

    deleted code

--- src/app/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Rubik } from "next/font/google";
-
-const rubik = Rubik({
-  weight: ["400", "500", "600"],
-  subsets: ["latin"],
-  variable: "--font-rubik", // Define the custom property name
-});
-
-export default rubik;
