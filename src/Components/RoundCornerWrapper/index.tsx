2f9f2f15c8675f76e4173c598115df2d58f33409
usmantariq <usman@alfain.tech>
2025-08-16 02:53:49 +0500

    design changes in services page

--- src/Components/RoundCornerWrapper/index.tsx
+++ src/Components/RoundCornerWrapper/index.tsx
@@ -2,10 +2,43 @@ import React from 'react';
 
 interface RoundCornerWrapperProps {
   children: React.ReactNode;
+  top?: boolean
+  bottom?: boolean
 }
 
-const RoundCornerWrapper: React.FC<RoundCornerWrapperProps> = ({ children }) => {
-  return (
+const RoundCornerWrapper: React.FC<RoundCornerWrapperProps> = ({ children,top=true,bottom=true }) => {
+
+  let wrapperObject = {
+    'topZero':  <div className="relative w-full  border border-[#373737] border-t-0">
+                <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -left-[5px]"></div>
+                <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -right-[5px]"></div>
+                {children}
+                </div>,
+
+    'bottomZero': <div className="relative w-full  border border-[#373737] border-b-0">
+                 <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -left-[5px]"></div>
+                 <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -right-[5px]"></div>
+                 {/* <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -left-[5px]"></div>
+                 <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -right-[5px]"></div> */}
+                     {children}
+               </div>  ,
+          'topBottomZero':     <div className="relative w-full  border border-[#373737] border-b-0">
+          {/* <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -left-[5px]"></div> */}
+          {/* <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -right-[5px]"></div> */}
+          {/* <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -left-[5px]"></div>
+          <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -right-[5px]"></div> */}
+              {children}
+        </div> 
+  }
+
+
+
+  return (<>
+  
+  {
+    top=== false && bottom === false ? wrapperObject['topBottomZero'] :
+    top === false ? wrapperObject['topZero'] : 
+    bottom === false ? wrapperObject['bottomZero'] :
     <div className="relative w-full  border border-[#373737]">
       <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -left-[5px]"></div>
       <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -top-[5px] -right-[5px]"></div>
@@ -13,7 +46,10 @@ const RoundCornerWrapper: React.FC<RoundCornerWrapperProps> = ({ children }) => 
       <div className="w-[9px] h-[9px] rounded-full bg-[#373737] absolute -bottom-[5px] -right-[5px]"></div>
           {children}
     </div>
-  );
+  
+  }
+    
+    </>);
 };
 
 export default RoundCornerWrapper;
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,80 +1,142 @@
-import React from 'react'
-import Box from '../Data/Box';
-import CardData from '@/Components/cardData/CardData';
-import TableData from '@/Components/tableData/TableData';
-import SmallCardData from '@/Components/SmallCardData';
-import RoundCornerWrapper from '@/Components/RoundCornerWrapper';
+import React from "react";
+import Box from "../Data/Box";
+import CardData from "@/Components/cardData/CardData";
+import TableData from "@/Components/tableData/TableData";
+import SmallCardData from "@/Components/SmallCardData";
+import RoundCornerWrapper from "@/Components/RoundCornerWrapper";
+import CommonQuestion from "@/Components/commonQuestion";
 const ServicesPage = () => {
   return (
-    <div className='text-white'>
-      <RoundCornerWrapper>
-        <div className='pl-15 pt-8'>
-          <span className='text-xs border-2 rounded'>{Box[0].boxhead}</span>
-          <h1 className='text-4xl w-100'>{Box[0].boxdes}</h1>
+    <div className="min-h-screen text-white">
+      <section>
+        <div className="w-full flex">
+          <div className="w-[10%] border-r border-b border-[#373737]"></div>
+          <div className="w-[79.95%] p-12">
+            <div className="w-40 mb-3">
+              <CommonQuestion question={Box[0].boxhead} />
+            </div>
+            <h1
+              className="text-5xl w-150"
+              style={{
+                background:
+                  "linear-gradient(92.95deg, rgba(248, 248, 248, 0.9) 0.1%, rgba(248, 248, 248, 0.5) 100.1%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+              {Box[0].boxdes}
+            </h1>
+          </div>
+          <div className="w-[10.05%] border-l border-b border-[#373737]"></div>
         </div>
-      </RoundCornerWrapper>
+      </section>
 
-      <RoundCornerWrapper>
-        <CardData />
-      </RoundCornerWrapper>
-      <RoundCornerWrapper>
-        <TableData />
-      </RoundCornerWrapper>
-      <RoundCornerWrapper>
-        <div className='pl-15 pt-8 mb-7'>
-          <span className='text-xs  rounded'>{Box[1].boxhead}</span>
-          <h1 className='text-4xl w-[170px]'>{Box[1].boxdes}</h1>
+      <section>
+  <div className="w-full flex">
+    <div className="w-[9.95%] border-b border-[#373737]"></div>
+    <div className="w-[80.08%]">
+      <CardData />
+    </div>
+    <div className="w-[9.95%] border-b border-[#373737]"></div>
+  </div>
+</section>
+
+<section>
+  <div className="w-full flex">
+    <div className="w-[9.95%] "></div>
+    <div className="w-[80.08%]">
+    <TableData />
+    </div>
+    <div className="w-[9.95%]"></div>
+    {/* <div className="w-[9.95%] border-b border-[#373737]"></div> */}
+  </div>
+</section>
+
+   
+<section>
+  <div className="w-full flex">
+    <div className="w-[9.95%] border-b border-[#373737]"></div>
+    
+    <div className="w-[80.08%]">
+    <RoundCornerWrapper top={false}>
+      <div className="p-6">
+        <div className="w-50 my-5 mt-15">
+         <CommonQuestion question={Box[1].boxhead} />
         </div>
-      </RoundCornerWrapper>
+      <h1
+              className="text-5xl w-70"
+              style={{
+                background:
+                  "linear-gradient(92.95deg, rgba(248, 248, 248, 0.9) 0.1%, rgba(248, 248, 248, 0.5) 100.1%)",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+              }}
+            >
+           {Box[1].boxdes}
+            </h1>
+      </div>
+    </RoundCornerWrapper>
+    </div>
+    <div className="w-[9.95%] border-b border-[#373737]"></div>
+  </div>
+</section>
+       
 
-      <div className='grid grid-cols-1  sm:flex  mx-8'>
-        <div className='w-full sm:w-1/2 '>
+
+      
+
+      <div className="grid grid-cols-1  sm:flex  mx-8">
+        <div className="w-full sm:w-1/2 ">
           <RoundCornerWrapper>
             <SmallCardData />
           </RoundCornerWrapper>
         </div>
-        <div className='w-full sm:w-1/2 '>
+        <div className="w-full sm:w-1/2 ">
           <div>
             <RoundCornerWrapper>
-              <div className='hidden  sm:flex flex-row  p-6'>
+              <div className="hidden  sm:flex flex-row  p-6">
                 <div>
-                  {
-                    Array.from({ length: 13 }, (_, index) => (
-                      <div key={index+'starrs'}>
-                        <span>*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *</span>
-                      </div>
-                    ))
-                  }
+                  {Array.from({ length: 13 }, (_, index) => (
+                    <div key={index + "starrs"}>
+                      <span>
+                        * * * * * * * * * * * * * * * * * * * * * * * * * * *
+                      </span>
+                    </div>
+                  ))}
                 </div>
-                <div className='ml-1'>
-                  {
-                    Array.from({ length: 13 }, (_, index) => (
-                      <div key={index+'starrsagain'}>
-                        <span>*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *</span>
-                      </div>
-                    ))
-                  }
+                <div className="ml-1">
+                  {Array.from({ length: 13 }, (_, index) => (
+                    <div key={index + "starrsagain"}>
+                      <span>
+                        * * * * * * * * * * * * * * * * * * * * * * * * * * *
+                      </span>
+                    </div>
+                  ))}
                 </div>
               </div>
             </RoundCornerWrapper>
 
-            <div className=' w-full   p-6 '>
+            <div className=" w-full   p-6 ">
               {
                 <RoundCornerWrapper>
                   <div>
-                    <span className='border-1 rounded-xl text-sm p-1'>{Box[2].boxhead}</span><br />
-                    <h1 className='text-4xl mt-2'>{Box[2].boxdes}</h1> <br />
-                    <button className='border rounded-2xl p-1 bg-orange-600'>More About us</button>
+                    <span className="border-1 rounded-xl text-sm p-1">
+                      {Box[2].boxhead}
+                    </span>
+                    <br />
+                    <h1 className="text-4xl mt-2">{Box[2].boxdes}</h1> <br />
+                    <button className="border rounded-2xl p-1 bg-orange-600">
+                      More About us
+                    </button>
                   </div>
                 </RoundCornerWrapper>
-
               }
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServicesPage
+export default ServicesPage;
